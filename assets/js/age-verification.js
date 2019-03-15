5a0183c3f1a88cbf1ef3a175b9cce1599a272b72
Thijs Smudde <contact@myroutes.io>
2019-03-15 13:14:50 +0100

    Added country selection to age verification

--- assets/js/age-verification.js
+++ assets/js/age-verification.js
@@ -30,16 +30,17 @@ av_showmodal = function() {
 	var modal_regret_wrapper = $('<div id="modal_regret_wrapper" class="content_wrapper" style="display:none;"></div>');
 
 	// Question Content
-	var content_heading = $('<h2>Are you 18 years or older?</h2>');
-	var content_buttons = $('<nav><ul><li><a href="#nothing" class="av_btn av_go" rel="yes">Yes</a></li><li><a href="#nothing" class="av_btn av_no" rel="no">No</a></li></nav>');
-	var content_text = $('<p>You must verify that you are 18 years of age or older to enter this site.</p>');
+  var content_country = $('<span class="countries"><h3>Which country are you from?</h3><a href="#" class="flaglink select_usa"><img src="./images/US.png" class="flag"/></a><a href="#" class="flaglink select_be"><img src="./images/BE.png" class="flaglink flag"/></a><a href="#" class="flaglink select_nl"><img src="./images/NL.png" class="flag"/></a>');
+  var content_heading = $('<h2 class="age_acceptance hidden">Are you <span class="beer_age">18</span> years or older?</h2></span>')
+  var content_buttons = $('<nav class="age_acceptance hidden"><ul><li><a href="#nothing" class="av_btn av_go" rel="yes">Yes</a></li><li><a href="#nothing" class="av_btn av_no" rel="no">No</a></li></nav>');
+  var content_text = $('<p class="age_acceptance hidden">You must verify that you are <span class="beer_age">18</span> years of age or older to enter this site.</p>');
 
 	// Regret Content
 	var regret_heading = $('<h2>We\'re Sorry.</h2>');
 	var regret_buttons = $('<nav><ul><li></li></ul></nav>');
-	var regret_text = $('<p>You must be 18 years of age or older to enter this site.</p>');
+	var regret_text = $('<p>You must be <span class="beer_age">18</span> years of age or older to enter this site.</p>');
 
-	modal_content_wrapper.append(content_heading, content_buttons, content_text);
+  modal_content_wrapper.append(content_country, content_heading, content_buttons, content_text);
 	modal_regret_wrapper.append(regret_heading, regret_buttons, regret_text);
 	modal_content.append(modal_content_wrapper, modal_regret_wrapper);
 
@@ -50,6 +51,9 @@ av_showmodal = function() {
 	av_positionPrompt();
 
 	modal_content.find('a.av_btn').on('click', av_setCookie);
+	modal_content.find('a.select_usa').on('click', av_selectUSA);
+	modal_content.find('a.select_be').on('click', av_selectBE);
+	modal_content.find('a.select_nl').on('click', av_selectNL);
 };
 
 av_setCookie = function(e) {
@@ -69,6 +73,47 @@ av_setCookie = function(e) {
 		av_showRegret();
 	}
 };
+
+const ages = {
+  usa: 21,
+  nl: 18,
+  be: 16
+}
+
+av_selectUSA = function(e) {
+  console.log('select USA')
+  setAge(ages['usa'])
+  hideAcceptance()
+}
+
+av_selectBE = function(e) {
+  console.log('select BE')
+  setAge(ages['be'])
+  hideAcceptance()
+}
+
+av_selectNL = function(e) {
+  console.log('select NL')
+  setAge(ages['nl'])
+  hideAcceptance()
+}
+
+setAge = function(age) {
+  const x = document.getElementsByClassName('beer_age')
+  for (var i = 0; i < x.length; i++) {
+    x[i].innerText = age
+  }
+}
+
+hideAcceptance = function() {
+    [].forEach.call(document.querySelectorAll(".age_acceptance"), function(el) {
+        el.classList.remove("hidden");
+    });
+
+    [].forEach.call(document.querySelectorAll(".countries"), function(el) {
+        el.classList.add("hidden");
+    });
+}
 
 av_closeModal = function() {
 	modal_content.fadeOut();
